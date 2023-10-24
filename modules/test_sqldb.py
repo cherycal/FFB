f35076579dd6def1af9170bcdd3167f203717ead
cherycal <chance_st@yahoo.com>
2023-10-23 21:59:20 -0700

    file locations

--- modules/test_sqldb.py
+++ modules/test_sqldb.py
@@ -9,9 +9,3 @@ fdb = sqldb.DB('Football.db')
 
 print(fdb.query("Select * from ESPNRosters"))
 
-# rows = bdb.query(cmd="select M.*, P.InjuryStatus as OldInjuryStatus from MostRecentPlayerData M, PlayerData P "
-#                    "where M.Date = P.Date and M.ESPNID = P.ESPNID")
-#
-# for row in rows:
-#     print(str(row['Name']) + ', ' + str(row['ESPNID']) + ', ' +
-#           str(row['InjuryStatus']) + ', ' + str(row['OldInjuryStatus']) )
