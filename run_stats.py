a4f8635ca0f57d4a19dd40d0cda7f7ae1b04e0f6
cherycal <chance_st@yahoo.com>
2023-11-02 11:13:04 -0700

    Cosmetic chgs

--- run_stats.py
+++ run_stats.py
@@ -1,16 +1,12 @@
 __author__ = 'chance'
 
 import sys
-sys.path.append('../modules')
+sys.path.append('./modules')
 sys.path.append('..')
 
 from player_stats import Stats
-
-
 def main():
-	stats = Stats()
-	print(stats)
-	stats.start(threaded=True, sleep_interval=240)
+	Stats().start(threaded=False, sleep_interval=240)
 
 
 if __name__ == "__main__":
